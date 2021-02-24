8bc7401ca76ff30f52592a55dd4e17cc3f5961fc
Diego <diegoteixeirapereira@hotmail.com>
2021-02-24 16:30:05 -0300

    criação da classe java rodar servidor

---
+++ javaChatMaster/src/br/com/java/RodaServidor.java
@@ -0,0 +1,13 @@
+package br.com.java;
+
+import java.io.IOException;
+
+public class RodaServidor {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		new Servidor(12345).executa();
+
+	}
+
+}
